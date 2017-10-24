69b14469f64fd3d02ea5b58900c0923efd0f7c8f
Ashvin <ashvinptel@gmail.com>
2017-10-24 13:05:40 +0530

    first commit

---
+++ app/controllers/api/v1/recipe_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::RecipeController < ApplicationController
+
+	# api :GET, '/recipes/:id'
+	# param :id, :name
+	# def show
+	#   # ...
+	# end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  apipie
+  resources :recipes
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, constraints: { format: 'json' } do
+  	namespace :v1 do
+  		# get "/recipes/:id" => "recipe#show"
+  		resources :recipes
+  	end
+  end
+
+  resources :charges
+end
